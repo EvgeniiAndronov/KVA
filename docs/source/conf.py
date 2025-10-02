6a7e7f99d189016fbc1c0496990603bea6b26011
sandra-smo <sasha27069@gmail.com>
2025-10-02 22:44:14 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'kva'
+project = 'kla'
 copyright = '2025, diana'
 author = 'diana'
 release = '1.0'
